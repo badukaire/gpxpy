fadde21aa3c99efa0efae3b57426516f6e4a2bd7
badukaire <badukaire@gmail.com>
2016-08-16 12:56:51 +0200

    check options: -n, -d, -h

--- marc.py
+++ marc.py
@@ -1,15 +1,98 @@
+import sys
+import getopt
 
+# local dir imports
 import gpxpy
 import gpxpy.gpx
+
+
+def eprint( sErrorMsg ) :
+  sys.stderr.write( sErrorMsg )
+  sys.stderr.write( "\n" )
+
+
+def usage() :
+  print "options :"
+  print "* -h : display this help text"
+  print "* -d <displayWhat>: what to display"
+  print "  <displayWhat> can be: routes, waypoints, tracks"
+  print "* -n <displayWhich>: which one to display (number starting by 1)"
+
+
+def checkOptions( pListParams ) :
+
+  global gsFileGPX
+  print( "checkOptions, args:", pListParams )
+  try:
+    lOptList, lList = getopt.getopt( pListParams, 'n:d:h' )
+
+  except getopt.GetoptError:
+    eprint( "FATAL : error analyzing command line options" )
+    eprint( "" )
+    usage()
+    sys.exit( 1 )
+
+  # TODO : use shift / setenv --
+
+  #print( lOptList )
+  #print( lList )
+  for lOpt in lOptList :
+    #print( "lOpt :" + str( lOpt ) )
+    if lOpt[0] == '-h' :
+      usage()
+      sys.exit( 0 )
+    if lOpt[0] == "-n" :
+      lsVal = lOpt[1]
+      try :
+        liVal = int( lsVal )
+      except :
+        eprint( "FATAL: %s not a valid number" % lsVal )
+        usage()
+        sys.exit( 1 )
+      if liVal > 0 :
+        global giWhich
+        giWhich = liVal
+      else :
+        eprint( "FATAL: %d must be >= 1" % liVal )
+        usage()
+        sys.exit( 1 )
+    if lOpt[0] == "-d" :
+      lsVal = lOpt[1]
+      if lsVal == "waypoints" :
+        global gbDispWaypts
+        gbDispWaypts = True
+      elif lsVal == "tracks" :
+        global gbDispTracks
+        gbDispTracks = True
+      elif lsVal == "routes" :
+        global gbDispRoutes
+        gbDispRoutes = True
+      else :
+        eprint( "FATAL: Invalid item to display %s" % lsVal )
+        eprint( "==========" )
+        usage()
+        sys.exit( 1 )
+
+  print "lList => ", lList
+  if len( lList ) > 0 :
+    gsFileGPX = lList[ 0 ]
+    print "will read file %s" % gsFileGPX
+
 
 def dispPoint( pPoint ) :
   print "Point at ({0},{1}) -> {2}".format( pPoint.latitude, pPoint.longitude, pPoint.elevation )
   print "-- timestamp:%s:" % pPoint.time
 
+
 def dispTracks() :
   liTrack = 0
   for lTrack in gXmlGPX.tracks:
     liTrack += 1
+    if giWhich > 0 :
+      print "giWhich = %d" % giWhich
+      if not liTrack == giWhich :
+        print "skip track #%d" % liTrack
+        continue
     print "============="
     print "track #%d name:%s:" % ( liTrack, lTrack.name )
     liSegment = 0
@@ -25,7 +108,8 @@ def dispTracks() :
     print "--------"
     print ""
 
-def dispWaypoints() :
+
+def dispWaypts() :
   liWaypt = 0
   for lWaypoint in gXmlGPX.waypoints:
     liWaypt += 1
@@ -36,6 +120,7 @@ def dispWaypoints() :
     print "-- description:", lWaypoint.description
     print "-- symbol:", lWaypoint.symbol
     
+
 def dispRoutes() :
   liRoute = 0
   for lRoute in gXmlGPX.routes:
@@ -46,15 +131,59 @@ def dispRoutes() :
       dispPoint( lPoint )
 
 
-# Parsing an existing file:
-# -------------------------
+gbDispTracks = False
+gbDispWaypts = False
+gbDispRoutes = False
+giWhich = 0
 
-lsFileGPX = "test_files/cerknicko-jezero.gpx"
-lFileGPX = open( lsFileGPX, "r" )
+liArgs = len( sys.argv )
+if liArgs > 1 :
+  gsFileGPX = None
+  checkOptions( sys.argv[ 1 : ] )
+  if not gsFileGPX == None :
+    lFileGPX = open( gsFileGPX, "r" )
+  else :
+    eprint( "NO GPX file provided, will use stdin" )
+    lFileGPX = sys.stdin
+else : # stdin
+  lFileGPX = sys.stdin
+  gbDispTracks = True
+  gbDispWaypts = True
+  gbDispRoutes = True
 
-gXmlGPX = gpxpy.parse( lFileGPX )
 
-dispTracks()
-#dispWaypoints()
-dispRoutes()
+try :
+  gXmlGPX = gpxpy.parse( lFileGPX )
+  lFileGPX.close()
+
+except :
+  eprint( "FATAL: Error parsing GPX data" )
+  sys.exit( 1 )
+
+if gbDispTracks == False and gbDispWaypts == False and gbDispRoutes == False :
+  print "NOT displaying anything"
+if gbDispTracks == True :
+  print "================"
+  print "display Tracks ..."
+  dispTracks()
+  print ""
+  print "Tracks displayed"
+  print "================"
+  print ""
+if gbDispWaypts == True :
+  print "================"
+  print "display Waypts ..."
+  dispWaypts()
+  print ""
+  print "Waypts displayed"
+  print "================"
+  print ""
+if gbDispRoutes == True :
+  print "================"
+  print "display Routes ..."
+  dispRoutes()
+  print ""
+  print "Routes displayed"
+  print "================"
+  print ""
 
